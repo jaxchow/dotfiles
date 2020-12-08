76d58942eaab36b9ebfe356e3ece4f701e62c66e
jaxchow <jaxchow@jaxchows-MacBook-Pro.local>
2020-12-08 20:16:55 +0800

    feat: add zsh config

--- brew.sh
+++ brew.sh
@@ -11,82 +11,16 @@ brew upgrade
 # Save Homebrew’s installed location.
 BREW_PREFIX=$(brew --prefix)
 
-# Install GNU core utilities (those that come with macOS are outdated).
-# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-# brew install coreutils
-# ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
-
-# Install some other useful utilities like `sponge`.
-# brew install moreutils
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
-# brew install findutils
-# Install GNU `sed`, overwriting the built-in `sed`.
-# brew install gnu-sed --with-default-names
-# Install a modern version of Bash.
-# brew install bash
-# brew install bash-completion2
-
-# Switch to using brew-installed bash as default shell
-# if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
-#   echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
-#   chsh -s "${BREW_PREFIX}/bin/bash";
-# fi;
-
-# Install `wget` with IRI support.
-brew install wget 
-
-
 # Install more recent versions of some macOS tools.
-brew install vim 
-brew install grep
-brew install openssh
-brew install screen
-
-
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-# brew install aircrack-ng
-# brew install bfg
-# brew install binutils
-# brew install binwalk
-# brew install cifer
-# brew install dex2jar
-# brew install dns2tcp
-# brew install fcrackzip
-# brew install foremost
-# brew install hashpump
-# brew install hydra
-# brew install john
-# brew install knock
-# brew install netpbm
-# brew install nmap
-# brew install pngcheck
-# brew install socat
-# brew install sqlmap
-# brew install tcpflow
-# brew install tcpreplay
-# brew install tcptrace
-# brew install ucspi-tcp # `tcpserver` etc.
-# brew install xpdf
-# brew install xz
+brew install vim --with-override-system-vi
 
 # Install other useful binaries.
-# brew install ack
-# brew install exiv2
-# brew install git
-# brew install git-lfs
-# brew install gs
-# brew install imagemagick --with-webp
-# brew install lua
-# brew install lynx
-# brew install p7zip
-# brew install pigz
-# brew install pv
-# brew install rename
-# brew install rlwrap
-# brew install ssh-copy-id
-# brew install tree
-# brew install vbindiff
-# brew install zopfli
 
+#brew install docker-edge
+#brew install google-chrome
+#brew install tunnelblick
+brew install v2rayx
+brew install visual-studio-code
+#brew install wpsoffice
 # Remove outdated versions from the cellar.
-brew cleanup
+#brew cleanup
